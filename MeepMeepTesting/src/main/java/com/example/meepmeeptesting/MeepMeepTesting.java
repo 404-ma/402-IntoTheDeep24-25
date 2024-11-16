ed92a9627f84a89447918f7f98572bb1672b291e
Arnav Choubey <nhz06x4og@mozmail.com>
2024-11-16 12:00:52 -0500

    Allow same class to simulate both autons

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,20 +1,32 @@
 package com.example.meepmeeptesting;
 
+import com.example.auton.Auton;
 import com.example.auton.AutonBlue;
+import com.example.auton.AutonRed;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.util.Objects;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-
-        AutonBlue auton = new AutonBlue();
+        Auton auton;
+        if(Objects.equals(args[0], "blue")){
+            auton = new AutonBlue();
+        } else if (Objects.equals(args[0], "red")) {
+            auton = new AutonRed();
+        }
+        else{
+            throw new RuntimeException("No Team Specified; Unable to start");
+        }
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .setColorScheme(new ColorSchemeBlueDark())
+                .setColorScheme(auton.getClass() == AutonBlue.class ? new ColorSchemeBlueDark() : new ColorSchemeRedDark())
                 .setStartPose(auton.getStartingPose())
                 .build();
 
