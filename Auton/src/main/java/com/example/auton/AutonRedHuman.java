af0a854a9f502ec92f91bd8efb1273fb43430362
Arnav Choubey <nhz06x4og@mozmail.com>
2024-12-21 11:31:50 -0500

    Get RedHuman working

--- Auton/src/main/java/com/example/auton/AutonRedHuman.java
+++ Auton/src/main/java/com/example/auton/AutonRedHuman.java
@@ -6,14 +6,25 @@ import com.acmerobotics.roadrunner.Vector2d;
 // The Auton used when playing red, and you want to move the three yellow samples into the basket zone
 public class AutonRedHuman implements Auton {
     public void Run(Runner runner) {
-        runner.move(b -> b.strafeToConstantHeading(new Vector2d(36, -8)));
-        runner.move(b -> b.strafeToConstantHeading(new Vector2d(54, -16)));
-        runner.move(b -> b.waitSeconds(0.1).lineToYConstantHeading(-60));
-        runner.move(b -> b.lineToYConstantHeading(-8));
-        runner.move(b -> b.strafeToConstantHeading(new Vector2d(62.5, -16)));
-        runner.move(b -> b.waitSeconds(0.1).lineToYConstantHeading(-60));
+        runner.move(b -> b.lineToY(-48));
+        runner.move(b -> b.waitSeconds(0.1).strafeTo(new Vector2d(36, -48)));
+        runner.move(b -> b.waitSeconds(0.1).lineToY(-8));
+        runner.move(b -> b.turnTo(Math.toRadians(90)));
+        runner.move(b -> b.strafeToConstantHeading(new Vector2d(50, -16)));
+        runner.move(b -> b.waitSeconds(0.1).lineToYConstantHeading(-54));
+        runner.move(b -> b.turnTo(Math.toRadians(135)));
+        runner.move(b -> b.lineToX(56));
+        runner.move(b -> b.strafeTo(new Vector2d(36, -8)));
+        runner.move(b -> b.turnTo(Math.toRadians(90)));
+        runner.move(b -> b.strafeToConstantHeading(new Vector2d(58, -16)));
+        runner.move(b -> b.waitSeconds(0.1).lineToYConstantHeading(-54));
+        runner.move(b -> b.turnTo(Math.toRadians(135)));
+        runner.move(b -> b.strafeTo(new Vector2d(36, -8)));
+        runner.move(b -> b.turnTo(Math.toRadians(90)));
+        runner.move(b -> b.strafeToConstantHeading(new Vector2d(66.5, -16)));
+        runner.move(b -> b.waitSeconds(0.1).lineToYConstantHeading(-54));
     }
     public Pose2d getStartingPose() {
-        return new Pose2d(12, -64, Math.toRadians(90));
+        return new Pose2d(12, -64, Math.toRadians(270));
     }
 }
